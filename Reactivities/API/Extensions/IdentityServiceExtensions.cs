1b3c4db49bc640622ce5fcc01b9bf1f16a807f4c
Ihor Arkhypenko <wasbornindecember@gmail.com>
2024-02-06 21:48:42 +0200

    Auth policy created. Token now stored in appsettings file.

--- Reactivities/API/Extensions/IdentityServiceExtensions.cs
+++ Reactivities/API/Extensions/IdentityServiceExtensions.cs
@@ -1,5 +1,8 @@
-﻿using API.Services;
+﻿using System.Text;
+using API.Services;
 using Domain;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Persistence;
 
 namespace API.Extensions;
@@ -8,13 +11,24 @@ public static class IdentityServiceExtensions
 {
     public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
     {
-        services.AddIdentityCore<AppUser>(opt =>
-        {
-            opt.Password.RequireNonAlphanumeric = false;
-        })
-        .AddEntityFrameworkStores<DataContext>();
+        services.AddIdentityCore<AppUser>(opt => { opt.Password.RequireNonAlphanumeric = false; })
+            .AddEntityFrameworkStores<DataContext>();
 
-        services.AddAuthentication();
+        var key = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(config["TokenKey"]));
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(opt =>
+            {
+                opt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = key,
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
         services.AddScoped<TokenService>();
 
         return services;
