bd5c004533f45695f98685e191eb54ff234892d2
Ihor Arkhypenko <wasbornindecember@gmail.com>
2024-02-10 18:37:22 +0200

    AttendanceHandler ready. Now user can attend or quit the activity. Host can cancel activity.

--- Reactivities/Application/Activities/ActivityDto.cs
+++ Reactivities/Application/Activities/ActivityDto.cs
@@ -19,6 +19,8 @@ public class ActivityDto
     public string Venue { get; set; }
 
     public string HostUsername { get; set; }
+    
+    public bool IsCancelled { get; set; }
 
     public ICollection<Profile> Attendees { get; set; }
 }
