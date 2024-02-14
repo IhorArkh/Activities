2f4d71d3dbf42e24fe29a96bf72ceeed8080441d
Ihor Arkhypenko <wasbornindecember@gmail.com>
2024-02-14 20:00:55 +0200

    Add photo(to cloudinary) logic ready.

--- Reactivities/API/Extensions/ApplicationServiceExtensions.cs
+++ Reactivities/API/Extensions/ApplicationServiceExtensions.cs
@@ -4,6 +4,7 @@ using Application.Interfaces;
 using FluentValidation;
 using FluentValidation.AspNetCore;
 using Infrastructure;
+using Infrastructure.Photos;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -33,6 +34,8 @@ public static class ApplicationServiceExtensions
         services.AddValidatorsFromAssemblyContaining<Create>();
         services.AddHttpContextAccessor();
         services.AddScoped<IUserAccessor, UserAccessor>();
+        services.AddScoped<IPhotoAccessor, PhotoAccessor>();
+        services.Configure<CloudinarySettings>(config.GetSection("Cloudinary"));
 
         return services;
     }
