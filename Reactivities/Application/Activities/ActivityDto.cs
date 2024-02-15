c63c6faeeb1af41b2b0dfacb43a5464fc9da2523
Ihor Arkhypenko <wasbornindecember@gmail.com>
2024-02-15 23:03:14 +0200

    Returning user profiles ready. Mapping profiles updated, AttendeeDto created.

--- Reactivities/Application/Activities/ActivityDto.cs
+++ Reactivities/Application/Activities/ActivityDto.cs
@@ -22,5 +22,5 @@ public class ActivityDto
     
     public bool IsCancelled { get; set; }
 
-    public ICollection<Profile> Attendees { get; set; }
+    public ICollection<AttendeeDto> Attendees { get; set; }
 }
