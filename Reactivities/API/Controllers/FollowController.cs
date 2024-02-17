4a9e4cf069edd11919b34f1d28abb9d5f5219e6a
Ihor Arkhypenko <wasbornindecember@gmail.com>
2024-02-17 18:21:35 +0200

    Controller and handler for following ready.

---
+++ Reactivities/API/Controllers/FollowController.cs
@@ -0,0 +1,13 @@
+﻿using Application.Followers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class FollowController : BaseApiController
+{
+    [HttpPost("{userName}")]
+    public async Task<IActionResult> Follow(string userName)
+    {
+        return HandleResult(await Mediator.Send(new FollowToggle.Command() { TargetUsername = userName }));
+    }
+}
